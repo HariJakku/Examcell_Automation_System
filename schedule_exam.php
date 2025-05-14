5d64721f76ca537e20f7fd19b94dd2b44c4f231a
Jakku Kumarswami <harijakku2005@gmail.com>
2025-05-14 18:41:57 +0530

    Update schedule_exam.php

--- schedule_exam.php
+++ schedule_exam.php
@@ -130,7 +130,7 @@ $exams_result = $conn->query($exams_query);
             </div>
         </div>
     </div>
-    <footer class="bg-gray-800 text-white text-center py-2 w-full fixed bottom-0 left-0">&copy; 2025 Gorli Laxmi</footer>
+    <footer class="bg-gray-800 text-white text-center py-2 w-full fixed bottom-0 left-0">&copy; 2025 Hari Jakku</footer>
 </body>
 </html>
 
